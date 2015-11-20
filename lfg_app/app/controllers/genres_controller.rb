f7b7f0ebcd504562e670ba774598e992af90ef6a
danielle <dani.zaghian@gmail.com>
2015-11-19 18:32:22 -0800

    changed views, added modals, added nav that functions

--- lfg_app/app/controllers/genres_controller.rb
+++ lfg_app/app/controllers/genres_controller.rb
@@ -2,30 +2,36 @@ class GenresController < ApplicationController
   def new
   	@genre = Genre.new
   end
+
   def create
   	genre_params = params.require(:genre).permit(:name)
   	@genre = Genre.create(genre_params)
-  	redirect_to @genre
+  	redirect_to genres_path
   end
-def index
+
+  def index
   	@genres = Genre.all
   end
 
   def show
-  	@genre = Genre.find(params[:id])
+  	@genre = Genre.friendly.find(params[:id])
   end
+
   def edit 
-  	@genre = Genre.find(params[:id])
+  	@genre = Genre.friendly.find(params[:id])
   end
+
   def update
   	genre_params = params.require(:genre).permit(:name)
-  	@genre = Genre.find(params[:id])
+  	@genre = Genre.friendly.find(params[:id])
   	@genre.update_attributes(genre_params)
   	redirect_to @genre
   end
+
   def destroy
-  	@genre = Genre.find(params[:id])
+  	@genre = Genre.friendly.find(params[:id])
   	@genre.destroy
   	redirect_to "/genres"
   end
+
 end
